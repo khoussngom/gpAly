8084e78a1f62fe96f76537442486de289e2fd527
khoussngom <khoussn@gmail.com>
2025-08-05 10:50:57 +0000

    ok

--- dist/test.js
+++ dist/test.js
@@ -6,7 +6,7 @@ const Maritime_1 = require("./models/Maritime");
 const produit1 = new Chimique_1.Chimique("Acide chlorhydrique", 3.5, 9);
 console.log(produit1.info());
 const produit = new Chimique_1.Chimique("Acide", 10, 5);
-const cargaison = new Maritime_1.Maritime(produit1, 1000); // Distance de 1000 km par exemple
+const cargaison = new Maritime_1.Maritime(produit1, 1000);
 console.log("Frais pour produit :", cargaison.calculerFrais("chimique", 10));
 let a = new Chimique_1.Chimique("Soude", 5, 2);
 console.log(a.info());
